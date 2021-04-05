62825c2733a18264fe01c906c8919ee0cf305adf
Fadeyev2000 <fda2000@ua.fm>
2021-04-05 22:03:41 +0300

    Исправляет map на forEach

--- js/task-01.js
+++ js/task-01.js
@@ -3,7 +3,7 @@ const itemsEl = document.querySelectorAll('li.item');
 console.log(`В списке ${itemsEl.length} категории.`);
 
 
-Array.from(itemsEl).map(option => {
+Array.from(itemsEl).forEach(option => {
     const nestedItemsEl = option.querySelectorAll('li.item li');
     
     console.log(`Категория: ${option.firstElementChild.textContent}`);
