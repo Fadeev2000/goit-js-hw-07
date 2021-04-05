24263cf2021006463159764b02cd14d3babfba74
Fadeyev2000 <fda2000@ua.fm>
2021-04-05 19:24:38 +0300

    Выводит в консоль текст заголовков (h2) и количество вложенных элементов li

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,14 @@
-const elItems = document.querySelectorAll('.item');
-console.log(`В списке ${elItems.length} категории.`);
+const itemsEl = document.querySelectorAll('li.item');
+
+console.log(`В списке ${itemsEl.length} категории.`);
+
+
+Array.from(itemsEl).map(option => {
+    const nestedItemsEl = option.querySelectorAll('li.item li');
+    
+    console.log(`Категория: ${option.firstElementChild.textContent}`);
+    console.log(`Количество элементов: ${nestedItemsEl.length}`);
+});
+
+
+
