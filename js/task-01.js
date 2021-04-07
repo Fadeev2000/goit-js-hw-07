d1217280c0b89729c93a757653e5a9aa52c7ddf8
Fadeyev2000 <fda2000@ua.fm>
2021-04-07 21:42:20 +0300

    Убирает метод from

--- js/task-01.js
+++ js/task-01.js
@@ -2,13 +2,9 @@ const itemsEl = document.querySelectorAll('li.item');
 
 console.log(`В списке ${itemsEl.length} категории.`);
 
-
-Array.from(itemsEl).forEach(option => {
+itemsEl.forEach(option => {
     const nestedItemsEl = option.querySelectorAll('li.item li');
     
     console.log(`Категория: ${option.firstElementChild.textContent}`);
     console.log(`Количество элементов: ${nestedItemsEl.length}`);
 });
-
-
-
